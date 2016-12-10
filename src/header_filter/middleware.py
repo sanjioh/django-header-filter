234aa8b513986ccc4222471a455d0228b1fb9adb
Fabio Sangiovanni <sjh+github@sanjioh.org>
2016-12-10 20:07:48 +0100

    enforce: implement header groups in rules

--- src/header_filter/middleware.py
+++ src/header_filter/middleware.py
@@ -9,13 +9,30 @@ class HeaderFilterMiddleware(MiddlewareMixin):
     def _reject(self, status, reason):
         return HttpResponse(status=status, reason=reason)
 
-    def process_request(self, request):
-        group = settings.HEADER_FILTER_RULES[0]['HEADERS']
-        rule_header, rule_value = settings.HEADER_FILTER_GROUPS[group][0]
+    def _all(self, group, request):
+        rule_headers, _ = zip(*group)
+        rule_headers = set(rule_headers)
+        request_headers = set(request.META.keys())
+
+        if not rule_headers.issubset(request_headers):
+            return False
+
+        for header, value in group:
+            if callable(value):
+                if not value(request):
+                    return False
+            elif request.META[header] != value:
+                return False
+
+        return True
+
+    def _do_enforce(self, rule, request):
+        group_name = settings.HEADER_FILTER_RULES[0]['HEADERS']
+        group = settings.HEADER_FILTER_GROUPS[group_name]
         status, reason = settings.HEADER_FILTER_RULES[0]['RESPONSE']
-        if rule_header not in request.META:
-            return self._reject(status, reason)
-        if callable(rule_value):
-            return None if rule_value(request) else self._reject(status, reason)
-        if request.META[rule_header] != rule_value:
-            return self._reject(status, reason)
+        return None if self._all(group, request) else self._reject(status, reason)
+
+    def process_request(self, request):
+        rule = settings.HEADER_FILTER_RULES[0]
+        action = settings.HEADER_FILTER_RULES[0]['ACTION']
+        return getattr(self, '_do_' + action)(rule, request)
--- tests/test_enforce.py
+++ tests/test_enforce.py
@@ -77,3 +77,90 @@ def test_enforce_single_callable_vs_wrong_value(settings, rf):
     res = hf.process_request(request)
     assert res.status_code == 400
     assert res.reason_phrase == 'enforce rule'
+
+
+def test_enforce_group_vs_missing(settings, rf):
+    settings.HEADER_FILTER_GROUPS = {
+        'group_a': (
+            ('HTTP_X_H_1', 'value1'),
+            ('HTTP_X_H_2', 'unused2'),
+            ('HTTP_X_H_3', 'unused3'),
+        ),
+    }
+    hf = mw.HeaderFilterMiddleware()
+    request = rf.get('/', HTTP_X_H_1='value1')
+    res = hf.process_request(request)
+    assert res.status_code == 400
+    assert res.reason_phrase == 'enforce rule'
+
+
+def test_enforce_group_literal_vs_ok(settings, rf):
+    settings.HEADER_FILTER_GROUPS = {
+        'group_a': (
+            ('HTTP_X_H_1', 'value1'),
+            ('HTTP_X_H_2', 'value2'),
+            ('HTTP_X_H_3', 'value3'),
+        ),
+    }
+    hf = mw.HeaderFilterMiddleware()
+    request = rf.get('/', HTTP_X_H_1='value1', HTTP_X_H_2='value2', HTTP_X_H_3='value3')
+    res = hf.process_request(request)
+    assert res is None
+
+
+def test_enforce_group_literal_vs_wrong_value(settings, rf):
+    settings.HEADER_FILTER_GROUPS = {
+        'group_a': (
+            ('HTTP_X_H_1', 'value1'),
+            ('HTTP_X_H_2', 'value2'),
+            ('HTTP_X_H_3', 'value3'),
+        ),
+    }
+    hf = mw.HeaderFilterMiddleware()
+    request = rf.get('/', HTTP_X_H_1='value1', HTTP_X_H_2='value2', HTTP_X_H_3='wrong_value')
+    res = hf.process_request(request)
+    assert res.status_code == 400
+    assert res.reason_phrase == 'enforce rule'
+
+
+def test_enforce_group_callable_vs_ok(settings, rf):
+    settings.HEADER_FILTER_GROUPS = {
+        'group_a': (
+            ('HTTP_X_H_1', lambda request: request.META['HTTP_X_H_1'] == 'value1'),
+            ('HTTP_X_H_2', lambda request: request.META['HTTP_X_H_2'] == 'value2'),
+            ('HTTP_X_H_3', lambda request: request.META['HTTP_X_H_3'] == 'value3'),
+        ),
+    }
+    hf = mw.HeaderFilterMiddleware()
+    request = rf.get('/', HTTP_X_H_1='value1', HTTP_X_H_2='value2', HTTP_X_H_3='value3')
+    res = hf.process_request(request)
+    assert res is None
+
+
+def test_enforce_group_callable_vs_wrong_value(settings, rf):
+    settings.HEADER_FILTER_GROUPS = {
+        'group_a': (
+            ('HTTP_X_H_1', lambda request: request.META['HTTP_X_H_1'] == 'value1'),
+            ('HTTP_X_H_2', lambda request: request.META['HTTP_X_H_2'] == 'value2'),
+            ('HTTP_X_H_3', lambda request: request.META['HTTP_X_H_3'] == 'value3'),
+        ),
+    }
+    hf = mw.HeaderFilterMiddleware()
+    request = rf.get('/', HTTP_X_H_1='value1', HTTP_X_H_2='value2', HTTP_X_H_3='wrong_value')
+    res = hf.process_request(request)
+    assert res.status_code == 400
+    assert res.reason_phrase == 'enforce rule'
+
+
+def test_enforce_group_mixed_vs_ok(settings, rf):
+    settings.HEADER_FILTER_GROUPS = {
+        'group_a': (
+            ('HTTP_X_H_1', lambda request: request.META['HTTP_X_H_1'] == 'value1'),
+            ('HTTP_X_H_2', 'value2'),
+            ('HTTP_X_H_3', lambda request: request.META['HTTP_X_H_3'] == 'value3'),
+        ),
+    }
+    hf = mw.HeaderFilterMiddleware()
+    request = rf.get('/', HTTP_X_H_1='value1', HTTP_X_H_2='value2', HTTP_X_H_3='value3')
+    res = hf.process_request(request)
+    assert res is None
