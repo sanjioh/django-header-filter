34152f90ae87b7c5774693a0dac875c649bafaa2
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-05-19 17:50:31 +0200

    Change actions to return None or a Django Response.

--- src/header_filter/actions.py
+++ src/header_filter/actions.py
@@ -1,14 +1,23 @@
-class Enforce:
-    def __init__(self, matcher):
-        self._matcher = matcher
+from django.http import HttpResponseBadRequest
 
-    def allow(self, request):
-        return self._matcher.match(request)
+
+class Enforce:
+    def __init__(self, matcher, reject_response=None):
+        self._matcher = matcher
+        self._reject_response = reject_response or HttpResponseBadRequest()
+
+    def process(self, request):
+        if self._matcher.match(request):
+            return None
+        return self._reject_response
 
 
 class Forbid:
-    def __init__(self, matcher):
+    def __init__(self, matcher, reject_response=None):
         self._matcher = matcher
+        self._reject_response = reject_response or HttpResponseBadRequest()
 
-    def allow(self, request):
-        return not self._matcher.match(request)
+    def process(self, request):
+        if not self._matcher.match(request):
+            return None
+        return self._reject_response
--- tests/test_actions.py
+++ tests/test_actions.py
@@ -1,32 +1,63 @@
+from django.http import HttpResponse, HttpResponseNotFound
+
 from header_filter.actions import Enforce, Forbid
 from header_filter.matchers import Header
 
 
-def test_enforce_allow(rf):
+def test_enforce_with_good_request(rf):
     h_name, h_value = 'HTTP_X_A', 'val_x'
     matcher = Header(h_name, h_value)
     action = Enforce(matcher)
     request = rf.get('/', **{h_name: h_value})
-    assert action.allow(request) is True
+    assert action.process(request) is None
 
 
-def test_enforce_deny(rf):
+def test_enforce_with_bad_request_and_default_response(rf):
     matcher = Header('HTTP_X_A', 'val_x')
     action = Enforce(matcher)
     request = rf.get('/', **{'HTTP_X_B': 'val_y'})
-    assert action.allow(request) is False
+    response = action.process(request)
+    assert isinstance(response, HttpResponse)
+    assert response.reason_phrase == 'BAD REQUEST'
+    assert response.status_code == 400
 
 
-def test_forbid_allow(rf):
+def test_enforce_with_bad_request_and_custom_response(rf):
+    matcher = Header('HTTP_X_A', 'val_x')
+    reject_reason = 'some reason'
+    action = Enforce(matcher, reject_response=HttpResponseNotFound(reason=reject_reason))
+    request = rf.get('/', **{'HTTP_X_B': 'val_y'})
+    response = action.process(request)
+    assert isinstance(response, HttpResponse)
+    assert response.reason_phrase == reject_reason
+    assert response.status_code == 404
+
+
+def test_forbid_with_good_request(rf):
+    matcher = Header('HTTP_X_A', 'val_x')
+    action = Forbid(matcher)
+    request = rf.get('/', **{'HTTP_X_B': 'val_y'})
+    assert action.process(request) is None
+
+
+def test_forbid_with_bad_request_and_default_response(rf):
     h_name, h_value = 'HTTP_X_A', 'val_x'
     matcher = Header(h_name, h_value)
     action = Forbid(matcher)
     request = rf.get('/', **{h_name: h_value})
-    assert action.allow(request) is False
+    response = action.process(request)
+    assert isinstance(response, HttpResponse)
+    assert response.reason_phrase == 'BAD REQUEST'
+    assert response.status_code == 400
 
 
-def test_forbid_deny(rf):
-    matcher = Header('HTTP_X_A', 'val_x')
-    action = Forbid(matcher)
-    request = rf.get('/', **{'HTTP_X_B': 'val_y'})
-    assert action.allow(request) is True
+def test_forbid_with_bad_request_and_custom_response(rf):
+    h_name, h_value = 'HTTP_X_A', 'val_x'
+    matcher = Header(h_name, h_value)
+    reject_reason = 'some reason'
+    action = Forbid(matcher, reject_response=HttpResponseNotFound(reason=reject_reason))
+    request = rf.get('/', **{h_name: h_value})
+    response = action.process(request)
+    assert isinstance(response, HttpResponse)
+    assert response.reason_phrase == reject_reason
+    assert response.status_code == 404
