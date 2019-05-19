7b1c3527b679cbcdff3a52fa2210d843a9b6f4e3
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-05-19 18:54:33 +0200

    Add middleware logic.

--- src/header_filter/middleware.py
+++ src/header_filter/middleware.py
@@ -4,15 +4,11 @@ from django.conf import settings
 class HeaderFilterMiddleware:
     def __init__(self, get_response):
         self.get_response = get_response
-        # One-time configuration and initialization.
 
     def __call__(self, request):
-        # Code to be executed for each request before
-        # the view (and later middleware) are called.
-
-        response = self.get_response(request)
-
-        # Code to be executed for each request/response after
-        # the view is called.
-
-        return response
+        rules = getattr(settings, 'HEADER_FILTER_RULES', [])
+        for rule in rules:
+            response = rule.process(request)
+            if response:
+                return response
+        return self.get_response(request)
--- tests/test_middleware.py
+++ tests/test_middleware.py
@@ -1,9 +1,62 @@
-from header_filter import Enforce, Forbid, Header as H, HeaderFilterMiddleware
+from django.http import HttpResponse, HttpResponseForbidden
+
+from header_filter import Enforce, Forbid, Header, HeaderFilterMiddleware
 
 
-def test_middleware_enforce_allow(settings, rf):
-    settings.HEADER_FILTER_RULES = (
-        Enforce(H('HTTP_X_A', 'val_x')),
-    )
-    mw = HeaderFilterMiddleware()
-    request = rf.get('/')
+def test_no_rule_list_setting(settings, rf):
+    mw = HeaderFilterMiddleware(lambda request: HttpResponse())
+    request = rf.get('/', **{'HTTP_X_A': 'val_x'})
+    response = mw(request)
+    assert response.status_code == 200
+
+
+def test_empty_rule_list_setting(settings, rf):
+    settings.HEADER_FILTER_RULES = []
+    mw = HeaderFilterMiddleware(lambda request: HttpResponse())
+    request = rf.get('/', **{'HTTP_X_A': 'val_x'})
+    response = mw(request)
+    assert response.status_code == 200
+
+
+def test_one_rule_and_good_request(settings, rf):
+    h_name, h_value = 'HTTP_X_A', 'val_x'
+    settings.HEADER_FILTER_RULES = [
+        Enforce(Header(h_name, h_value)),
+    ]
+    mw = HeaderFilterMiddleware(lambda request: HttpResponse())
+    request = rf.get('/', **{h_name: h_value})
+    response = mw(request)
+    assert response.status_code == 200
+
+
+def test_one_rule_and_bad_request(settings, rf):
+    settings.HEADER_FILTER_RULES = [
+        Enforce(Header('HTTP_X_A', 'val_x')),
+    ]
+    mw = HeaderFilterMiddleware(lambda request: HttpResponse())
+    request = rf.get('/', **{'HTTP_X_B': 'val_y'})
+    response = mw(request)
+    assert response.status_code == 400
+
+
+def test_two_rules_and_bad_request_triggers_the_second(settings, rf):
+    settings.HEADER_FILTER_RULES = [
+        Enforce(Header('HTTP_X_A', 'val_x')),
+        Forbid(Header('HTTP_X_B', 'val_y')),
+    ]
+    mw = HeaderFilterMiddleware(lambda request: HttpResponse())
+    request = rf.get('/', **{'HTTP_X_A': 'val_x', 'HTTP_X_B': 'val_y'})
+    response = mw(request)
+    assert response.status_code == 400
+
+
+def test_first_violation_is_final(settings, rf):
+    settings.HEADER_FILTER_RULES = [
+        Enforce(Header('HTTP_X_A', 'val_x')),
+        Forbid(Header('HTTP_X_B', 'val_y'), reject_response=HttpResponseForbidden()),
+        Enforce(Header('HTTP_X_C', 'val_z')),
+    ]
+    mw = HeaderFilterMiddleware(lambda request: HttpResponse())
+    request = rf.get('/', **{'HTTP_X_A': 'val_x', 'HTTP_X_B': 'val_y'})
+    response = mw(request)
+    assert response.status_code == 403
