c9e42d2dfc2f3780c2d284a2c0622f1e25040293
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-05-21 21:44:40 +0200

    Test HeaderRegexp against re.Pattern objects.

--- tests/test_matcher_header_re.py
+++ tests/test_matcher_header_re.py
@@ -1,25 +1,51 @@
+import re
+
 from header_filter.matchers import HeaderRegexp
 
 
-def test_header_re_name_and_value_match(rf):
+def test_header_name_and_value_match_re_pattern(rf):
     matcher = HeaderRegexp(r'^HTTP_X_A.*$', r'^val_.$')
     request = rf.get('/', **{'HTTP_X_A_XYZ': 'val_x'})
     assert matcher.match(request) is True
 
 
-def test_header_re_name_doesnt_match(rf):
+def test_header_name_and_value_match_re_object(rf):
+    matcher = HeaderRegexp(re.compile(r'^HTTP_X_A.*$'), re.compile(r'^val_.$'))
+    request = rf.get('/', **{'HTTP_X_A_XYZ': 'val_x'})
+    assert matcher.match(request) is True
+
+
+def test_header_name_doesnt_match_re_pattern(rf):
     matcher = HeaderRegexp(r'^HTTP_X_A.*$', r'^val_.$')
     request = rf.get('/', **{'HTTP_X_B_XYZ': 'val_x'})
     assert matcher.match(request) is False
 
 
-def test_header_re_value_doesnt_match(rf):
+def test_header_name_doesnt_match_re_object(rf):
+    matcher = HeaderRegexp(re.compile(r'^HTTP_X_A.*$'), re.compile(r'^val_.$'))
+    request = rf.get('/', **{'HTTP_X_B_XYZ': 'val_x'})
+    assert matcher.match(request) is False
+
+
+def test_header_value_doesnt_match_re_pattern(rf):
     matcher = HeaderRegexp(r'^HTTP_X_A.*$', r'^val_.$')
     request = rf.get('/', **{'HTTP_X_A_XYZ': 'val_'})
     assert matcher.match(request) is False
 
 
-def test_header_re_name_and_value_dont_match(rf):
+def test_header_value_doesnt_match_re_object(rf):
+    matcher = HeaderRegexp(re.compile(r'^HTTP_X_A.*$'), re.compile(r'^val_.$'))
+    request = rf.get('/', **{'HTTP_X_A_XYZ': 'val_'})
+    assert matcher.match(request) is False
+
+
+def test_header_name_and_value_dont_match_re_pattern(rf):
     matcher = HeaderRegexp(r'^HTTP_X_A.*$', r'^val_.$')
+    request = rf.get('/', **{'HTTP_X_B_XYZ': 'val_'})
+    assert matcher.match(request) is False
+
+
+def test_header_name_and_value_dont_match_re_object(rf):
+    matcher = HeaderRegexp(re.compile(r'^HTTP_X_A.*$'), re.compile(r'^val_.$'))
     request = rf.get('/', **{'HTTP_X_B_XYZ': 'val_'})
     assert matcher.match(request) is False
