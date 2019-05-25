18480828568afd881713bab6cd71607fadf52265
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-05-25 21:28:29 +0200

    Pending XOR test.

--- tests/test_matcher_xor.py
+++ tests/test_matcher_xor.py
@@ -1,6 +1,15 @@
 from header_filter.matchers import Header
 
 
+def test_xor_followed_by_xor(rf):
+    h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
+    h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
+    h_name_3, h_value_3 = 'HTTP_X_C', 'val_z'
+    matcher = Header(h_name_1, h_value_1) ^ Header(h_name_2, h_value_2) ^ Header(h_name_3, h_value_3)
+    request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2, h_name_3: h_value_3})
+    assert matcher.match(request) is True
+
+
 # def test_or_followed_by_or(rf):
 #     h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
 #     h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
