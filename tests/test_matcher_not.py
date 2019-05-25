89288bcda1ecb03f848b5d6a34ea4fbae2abd15d
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-05-25 20:09:48 +0200

    Move not truth tables tests.

--- tests/test_matcher_not.py
+++ tests/test_matcher_not.py
@@ -1,19 +1,6 @@
 from header_filter.matchers import Header
 
 
-def test_not_true_equals_false(rf):
-    h_name, h_value = 'HTTP_X_A', 'val_x'
-    matcher = ~Header(h_name, h_value)
-    request = rf.get('/', **{h_name: h_value})
-    assert matcher.match(request) is False
-
-
-def test_not_false_equals_true(rf):
-    matcher = ~Header('HTTP_X_A', 'val_x')
-    request = rf.get('/', **{'HTTP_X_B': 'val_y'})
-    assert matcher.match(request) is True
-
-
 def test_double_not(rf):
     h_name, h_value = 'HTTP_X_A', 'val_x'
     matcher = ~~Header(h_name, h_value)
--- tests/test_matcher_truth_tables.py
+++ tests/test_matcher_truth_tables.py
@@ -5,6 +5,19 @@ import pytest
 from header_filter.matchers import Header
 
 
+def test_not_true_equals_false(rf):
+    h_name, h_value = 'HTTP_X_A', 'val_x'
+    matcher = ~Header(h_name, h_value)
+    request = rf.get('/', **{h_name: h_value})
+    assert matcher.match(request) is False
+
+
+def test_not_false_equals_true(rf):
+    matcher = ~Header('HTTP_X_A', 'val_x')
+    request = rf.get('/', **{'HTTP_X_B': 'val_y'})
+    assert matcher.match(request) is True
+
+
 @pytest.mark.parametrize('op,result', [(and_, True), (or_, True), (xor, False)], ids=['and', 'or', 'xor'])
 def test_true_op_true(op, result, rf):
     h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
