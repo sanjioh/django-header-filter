d991374cec60e73c2a16d0ade6972b73ed709b93
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-05-27 19:20:35 +0200

    xor tests

--- tests/test_matcher_not.py
+++ tests/test_matcher_not.py
@@ -22,3 +22,12 @@ def test_not_matcher_supports_bitwise_or(rf):
     matcher = ~Header(h_name_1, h_value_1) | Header(h_name_2, h_value_2)
     request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2})
     assert matcher.match(request) is True
+
+
+def test_not_matcher_supports_bitwise_xor(rf):
+    h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
+    h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
+    h_name_3, h_value_3 = 'HTTP_X_C', 'val_z'
+    matcher = ~Header(h_name_2, h_value_2) ^ Header(h_name_3, h_value_3)
+    request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2, h_name_3: h_value_3})
+    assert matcher.match(request) is True
--- tests/test_matcher_xor.py
+++ tests/test_matcher_xor.py
@@ -1,36 +1,36 @@
 from header_filter.matchers import Header
 
 
-def test_xor_followed_by_xor(rf):
+def test_xor_matcher_supports_bitwise_not(rf):
+    h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
+    h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
+    matcher = ~(Header(h_name_1, h_value_1) ^ Header(h_name_2, h_value_2))
+    request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2})
+    assert matcher.match(request) is True
+
+
+def test_xor_matcher_supports_bitwise_and(rf):
     h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
     h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
     h_name_3, h_value_3 = 'HTTP_X_C', 'val_z'
-    matcher = Header(h_name_1, h_value_1) ^ Header(h_name_2, h_value_2) ^ Header(h_name_3, h_value_3)
+    matcher = (Header(h_name_1, h_value_1) ^ Header(h_name_2, h_value_2)) & Header(h_name_3, h_value_3)
+    request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2, h_name_3: h_value_3})
+    assert matcher.match(request) is False
+
+
+def test_xor_matcher_supports_bitwise_or(rf):
+    h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
+    h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
+    h_name_3, h_value_3 = 'HTTP_X_C', 'val_z'
+    matcher = (Header(h_name_1, h_value_1) ^ Header(h_name_2, h_value_2)) | Header(h_name_3, h_value_3)
     request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2, h_name_3: h_value_3})
     assert matcher.match(request) is True
 
 
-# def test_or_followed_by_or(rf):
-#     h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
-#     h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
-#     h_name_3, h_value_3 = 'HTTP_X_C', 'val_z'
-#     matcher = Header(h_name_1, h_value_1) | Header(h_name_2, h_value_2) | Header(h_name_3, h_value_3)
-#     request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2, h_name_3: h_value_3})
-#     assert matcher.match(request) is True
-
-
-# def test_or_followed_by_and(rf):
-#     h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
-#     h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
-#     h_name_3, h_value_3 = 'HTTP_X_C', 'val_z'
-#     matcher = (Header(h_name_1, h_value_1) | Header(h_name_2, h_value_2)) & Header(h_name_3, h_value_3)
-#     request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2, h_name_3: h_value_3})
-#     assert matcher.match(request) is True
-
-
-# def test_or_of_not(rf):
-#     h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
-#     h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
-#     matcher = ~Header(h_name_1, h_value_1) | Header(h_name_2, h_value_2)
-#     request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2})
-#     assert matcher.match(request) is True
+def test_xor_matcher_supports_bitwise_xor(rf):
+    h_name_1, h_value_1 = 'HTTP_X_A', 'val_x'
+    h_name_2, h_value_2 = 'HTTP_X_B', 'val_y'
+    h_name_3, h_value_3 = 'HTTP_X_C', 'val_z'
+    matcher = (Header(h_name_1, h_value_1) ^ Header(h_name_2, h_value_2)) ^ Header(h_name_3, h_value_3)
+    request = rf.get('/', **{h_name_1: h_value_1, h_name_2: h_value_2, h_name_3: h_value_3})
+    assert matcher.match(request) is True
