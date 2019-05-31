21651120925cc3e51aeada4eac4dbfaa5bf98fae
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-05-31 20:43:13 +0200

    Allow HeaderRegexp to be imported directly from header_filter package.

--- src/header_filter/__init__.py
+++ src/header_filter/__init__.py
@@ -1,3 +1,3 @@
-from header_filter.matchers import Header  # noqa: F401
+from header_filter.matchers import Header, HeaderRegexp  # noqa: F401
 from header_filter.middleware import HeaderFilterMiddleware  # noqa: F401
 from header_filter.rules import Enforce, Forbid  # noqa: F401
