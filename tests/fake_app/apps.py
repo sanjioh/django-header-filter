ce239b251590b22577c2b93134b6f1fd5c73ef73
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-05-31 20:46:25 +0200

    Add fake django application.

---
+++ tests/fake_app/apps.py
@@ -0,0 +1,5 @@
+from django.apps import AppConfig
+
+
+class FakeAppConfig(AppConfig):
+    name = 'fake_app'
---
+++ tests/fake_app/urls.py
@@ -0,0 +1,7 @@
+from django.conf.urls import url
+
+from .views import fake_view
+
+urlpatterns = [
+    url(r'^testurl/$', fake_view, name='testurl'),
+]
