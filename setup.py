861adf5d9fc27c2a7ecfb7b05745282656b9924a
Fabio Sangiovanni <sjh+github@sanjioh.org>
2019-06-16 12:23:23 +0200

    Improve packaging.

--- setup.py
+++ setup.py
@@ -1,12 +1,3 @@
-from setuptools import find_packages, setup
+from setuptools import setup
 
-setup(
-    name='django-header-filter',
-    version='0.0.1',
-    packages=find_packages(where='src'),
-    package_dir={'': 'src'},
-    zip_safe=False,
-    install_requires=[
-        'Django>=1.11,<2.3',
-    ],
-)
+setup()
